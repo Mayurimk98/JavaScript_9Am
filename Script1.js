6ea9a768efb65134cadf3b0e8a3e02333945cf8e
Mayurimk98 <mayurimkatwe@gmail.com>
2023-01-02 09:17:40 +0530

    2jan2023

---
+++ Script1.js
@@ -0,0 +1,83 @@
+//batch
+//morning 8:30 am , evening 7 pm
+//support session => 10 am, 8 pm 
+
+//notebook 
+
+//JAVASCRIPT
+//java
+
+//print
+// console.log(`mayuri`)
+// console.log(300)
+
+// //"" , '', `` => string
+// console.log('shreeGanesh') //shreeGanesh
+
+// //print
+
+// console.log(10) //10
+// console.log('shubhangi') //shubhangi
+// console.log("Minskole") //Minskole
+// console.log(`Ganesh`) //Ganesh
+
+//mayuri => comments => user understanding
+
+//information store
+
+//keyword
+//let 
+//const
+
+// let a=10
+// //keyword varibalename = value
+
+// console.log(a) //10
+
+// a=20 //update
+// console.log(a) //20
+// console.log(a)
+
+// let firstName="vaibhav"
+// console.log(firstName)//vaibhav
+
+// firstName="radha" //update
+// console.log(firstName) //radha
+
+// const x=100
+// console.log(x) //100
+
+// const student="Sham"
+// console.log(student) //Sham
+// student ="Ram" //update
+// console.log(student) //error
+
+
+// let s=50
+// let v=20
+// console.log(s) //50
+// console.log(v) //20
+
+//operators
+//arithmatic
+//comparison
+//logical
+
+//arithmatic => addition, substraction, multiplication, divison, modulus
+// (+, -,*,/,%)
+
+let s=50
+let v=20
+console.log(s+v) //addition //70
+console.log(s-v) //30
+console.log(s*v) //1000
+console.log(s/v) //2.5
+console.log(s%v) //10
+
+let x=20
+let y=2
+console.log(x+y) //22
+console.log(x-y) //18
+console.log(x*y) //40
+console.log(x/y) //10
+console.log(x%y) //0
