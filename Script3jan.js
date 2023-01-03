c7da205295b45490920a4f642380253c38f99278
Mayurimk98 <mayurimkatwe@gmail.com>
2023-01-03 09:12:24 +0530

    3 jan

---
+++ Script3jan.js
@@ -0,0 +1,93 @@
+//switch case =>
+
+//loop
+//for loop
+//while loop
+//do while
+
+//function => DRY
+
+//loop => repetative operation perform based on condition
+
+//1 - 100000 => print
+
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+
+//1. for loop
+//syntax
+
+// for(initialization;conditioncheck;increment/decrement){
+//     //statements
+// }
+
+
+//let firstName="Mayuri"
+//1-5 print
+
+//forward loop
+
+// for (let i = 1; i <= 5; i++) { //2  3  4 5 6
+//     console.log(i)   //1 1<=5   2 2<=5  3 3<=5  4 4<=5 5 5<=5 6 6<=5
+// }
+//1
+//2
+//3
+//4
+//5
+
+//good morning => 5 time
+// for (let i = 1; i <= 5; i++) {
+//     console.log("Good morning")
+// }
+
+//reverse loop
+//5 - 1 
+// for (let i = 5; i >= 1; i--){ //4 3 2 1 0
+//     console.log(i) // 5 4 3 2 1
+// }
+
+// i=5 5>=1
+// i=4  4>=1
+//i =3 3>=1
+//i=2  2>=1
+//i=1 1>=1
+//i=0 0>=1
+
+
+//table of 5
+//5  =  50  
+
+// 5  => 5+ 5 =>10
+// 10
+// 15
+
+for (let i = 5; i <= 50; i = i + 5) { //5+5 =>10  10+5 15+5
+    console.log(i) //5 10 15 20-----50
+}
+
+//i =5 5<=50
+//i=10 10<=50 
+//i=15 15<=50
+//i=20 20<=50
+
+
+
+
+
+
+
+
+
+
+
+// let sum = 0//10
+// sum = sum + 10  //0+10 =>10
+// console.log(sum)
+
+
+
+
