5ec8b2c568d7a97b580238196c4e22758220b6a1
Mayurimk98 <mayurimkatwe@gmail.com>
2023-01-06 09:17:42 +0530

    6 jan

---
+++ Script6jan.js
@@ -0,0 +1,121 @@
+//loop
+
+//for loop
+//while loop
+
+//repetative
+//1000 time mayuri
+
+//5-10
+
+// for (let i = 5; i <= 10; i++) {
+//     console.log(i)
+// }
+
+
+//10-5
+// for (let i = 10; i >= 5; i--) {
+//     console.log(i)
+// }
+
+//12 table
+// for (let i = 12; i <= 120; i = i + 12) {
+//     console.log(i)
+// }
+
+
+//break
+// for (let i = 12; i <= 120; i = i + 12) {
+//     if (i == 60) {
+//         break
+//     }
+//     console.log(i)
+// }
+
+// for (let i = 12; i <= 120; i = i + 12) {
+//     if (i == 60) {
+//         continue
+//     }
+//     console.log(i)
+// }
+
+
+//1-10 even number or odd 
+// for (let i = 1; i <= 10; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i)
+//         console.log('this is even number')
+//     }
+//     else{
+//         console.log(i)
+//         console.log('this is odd number')
+//     }
+// }
+
+
+//while
+//1-10
+
+// let i = 1
+// while (i <= 10) {
+//     console.log(i)
+//     i++
+// }
+
+
+//10-5
+// let i = 10
+// while (i >= 5) {
+//     console.log(i)
+//     i--
+// }
+
+//7 table
+// let i = 7
+// while (i <= 70) {
+//     console.log(i)
+//     i = i + 7
+// }
+
+
+// let i = 7
+// while (i <= 70) {
+//     if (i == 42) {
+//         break
+//     }
+//     console.log(i)
+//     i = i + 7
+// }
+
+// let i = 7
+// while (i <= 70) {
+//     if (i == 49) {
+//         break
+//     }
+//     console.log(i)
+//     i = i + 7
+// }
+
+//1-10
+// let i = 1
+// while (i <= 10) {
+//     if (i == 5) {
+//         i++
+//         continue
+//     }
+//     console.log(i) //1 2 3 4
+//     i++
+// }
+
+
+//5 continue 7 break
+
+for (let i = 1; i <= 10; i++) {
+    if (i == 5) { //5==5 10
+        continue
+    }
+    if (i == 9) {  //9==9
+        break
+    }
+    console.log(i) //1 2 3 4 6 7 8 
+}
