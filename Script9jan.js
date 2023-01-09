a129d51f936fbe3c1ef1b0676af99c160c578c28
Mayurimk98 <mayurimkatwe@gmail.com>
2023-01-09 09:18:45 +0530

    9 jan2023

---
+++ Script9jan.js
@@ -0,0 +1,120 @@
+// //loop
+
+// //for loop
+
+// //good morning 5 times
+// // for(let i=1;i<=5;i++){
+// //     //console.log(i) //number
+// //     console.log('good morning')
+// // }
+
+// // let i=1
+// // while(i<=5){
+// //     console.log('good morning')
+// //     i++
+// // }
+
+// //====================================================================
+
+// // console.log(Math.floor(12.5)) //12
+
+// // console.log(Math.floor(46.8)) //46
+
+// // console.log(Math.floor(-36.89)) //-37
+
+// // //    -37    -36   -1  0 1 2 3
+
+// // console.log(Math.floor(-82.2)) //-83
+
+// //console.log(Math.floor(12))
+
+// // console.log(Math.ceil(12.5)) //13
+// // console.log(Math.ceil(45.1)) //46
+// // console.log(Math.ceil(-12.65)) //-12
+// // console.log(Math.ceil(-7.98)) //-7
+
+
+// // console.log(Math.round(12.65)) //13
+// // console.log(Math.round(5.24)) //5
+// // console.log(Math.round(-6.78)) //-7
+
+
+// //Math.floor(number)
+// //Math.ceil(number)
+// //Math.round(number) //0.5
+
+// // console.log(Math.random()) //0.00-0.99
+
+// //1-5 => random number
+// // console.log(Math.random()*5) //0.0-4.95
+
+// // console.log(Math.floor(4.95)) //4
+
+// //console.log(Math.floor(Math.random()*5))//0-4
+
+// //console.log(Math.floor(Math.random()*5)+1)//1-5
+
+
+// //1-5 random  //4 
+
+// //user input  //3
+
+// //guess is correct
+// //guess is incorrect
+
+// let randomNumber = Math.floor(Math.random() * 5) + 1
+// console.log(randomNumber) //1-5
+
+// let userInput = prompt("Enter any number between 1-5")
+
+// if (userInput == randomNumber) {
+//     console.log('your guess is correct!!!')
+// }
+// else {
+//     console.log('your guess is incorrect!!')
+// }
+
+
+
+
+//user input => prompt() => browser =>html
+
+//index.html
+//shift + !
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let a
+// console.log(a)
+
+// let b =null
+// console.log(b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
