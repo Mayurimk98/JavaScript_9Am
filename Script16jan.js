fff9f0e3d09aa116f2328d2539a5c7fa9c20c68a
Mayurimk98 <mayurimkatwe@gmail.com>
2023-01-16 09:12:22 +0530

    16 jan

---
+++ Script16jan.js
@@ -0,0 +1,159 @@
+
+// let a1 = 100
+// console.log(a1)
+// console.log(typeof a1)
+
+// let a2 = '100'
+// console.log(typeof a2)
+
+// let myName = "mayuri"
+// console.log(myName)
+// console.log(typeof myName)
+
+
+// let isDoingJob = true
+// console.log(typeof isDoingJob)
+
+// //let marks=20
+
+// // let marks=20,30,40
+
+// //Array => collection of multiple element
+
+// // let rollNumber = [20, 40, 50, 67, 89, 90]
+
+// //javascript
+// //Object
+// //property and method
+
+// //human
+// //propery => age, color, weight,height, name
+// //method =>talk(), walk(), sleep(), study()
+
+// //vehical
+// //property => color, model number, type
+// //method => start(), stop()
+
+
+// //Array => object
+// //Property
+// //Methods
+
+// //                 0   1   2   3   4   5       
+// let rollNumber = [20, 40, 50, 67, 89, 90] 
+// console.log(typeof rollNumber)
+
+// console.log(rollNumber)
+
+// //array store value with the help of index and index start with zero
+
+// console.log(rollNumber[0])
+// console.log(rollNumber[5])
+
+// //          0         1        2           3
+// let names=["Sneha","mayuri","harshada","mayuri"]
+// console.log(names[3])
+// console.log(names[1])
+
+
+//peroperty => length
+//method
+
+//index => 0
+//length => 1
+
+// let names=["Sneha","mayuri","harshada","mayuri"]
+// console.log(names.length)
+
+// let rollNumber = [20, 40, 50, 67, 89, 90] 
+// console.log(rollNumber.length)
+
+
+//method => study()
+//action => learn something
+//return => good marks/ good job
+
+//array
+//method
+
+//1. push()
+//action => add the element at last
+//return => new length
+
+// let rollNumber = [23, 45, 67, 89, 89]
+// console.log(rollNumber.length) //5
+
+// let a2=rollNumber.push(100)
+// console.log(a2) //6
+
+// console.log(rollNumber)
+
+//2. unshift()
+//action => add the element at first
+//return => new length
+
+// let home = ['1BHK', '2BHK', '3BHK']
+// let s1 = home.unshift("1RK")
+// console.log(s1)
+
+// console.log(home)
+
+
+//3. pop()
+//action => remove the last element
+// //return =>return the same removed element
+// let city = ["Pune", "Nashik", "Sangamner", "Bhagur"]
+// let r1=city.pop()
+// console.log(r1) //Bhagur
+
+// console.log(city)
+
+//4. shift()
+//action => remove the first element
+//return => return same removed element
+let city = ["Pune", "Nashik", "Sangamner", "Bhagur"]
+let r2 = city.shift()
+console.log(r2) //Pune
+console.log(city)
+
+
+
+
+
+
+
+
+
+
+
+//push(ele)
+//unshift(ele)
+//pop()
+//shift()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
